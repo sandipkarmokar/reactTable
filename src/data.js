d66ee332ae24557d2c7391a2c7139ce49af02d26
Sandip Karmokar <63592069+sandipkarmokar@users.noreply.github.com>
2023-04-11 14:42:39 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -10,6 +10,15 @@ const data = [
     },
     {
         id: 2,
+        name: 'Gayrish',
+        email: 'gayrish@gmail.com',
+        phone: 1800180180,
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vulputate ipsum orci ultrices pellentesque id sagittis.',
+        status: 'status',
+    },
+    {
+        id: 3,
         name: 'John Smith',
         email: 'john@gmail.com',
         phone: 1800180180,
@@ -18,7 +27,7 @@ const data = [
         status: 'status',
     },
     {
-        id: 3,
+        id: 4,
         name: 'Edward Kenway',
         email: 'edward@gmail.com',
         phone: 1800180180,
@@ -27,7 +36,7 @@ const data = [
         status: 'status',
     },
     {
-        id: 4,
+        id: 5,
         name: 'Mason Wood',
         email: 'mason@gmail.com',
         phone: 1800180180,
@@ -36,7 +45,7 @@ const data = [
         status: 'status',
     },
     {
-        id: 5,
+        id: 6,
         name: 'Frank Wood',
         email: 'frank@gmail.com',
         phone: 1800180180,
@@ -45,7 +54,7 @@ const data = [
         status: 'status',
     },
     {
-        id: 6,
+        id: 7,
         name: 'David',
         email: 'david@gmail.com',
         phone: 1800180180,
@@ -54,7 +63,7 @@ const data = [
         status: 'status',
     },
     {
-        id: 7,
+        id: 8,
         name: 'George',
         email: 'george@gmail.com',
         phone: 1800180180,
@@ -63,7 +72,7 @@ const data = [
         status: 'status',
     },
     {
-        id: 8,
+        id: 9,
         name: 'Frenkie',
         email: 'frenkie@gmail.com',
         phone: 1800180180,
